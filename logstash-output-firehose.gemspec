67b10088d3d86d8f3b1aef35b03ff9a130e84687
Patrick Robinson <patrick.robinson@envato.com>
2018-02-01 15:18:21 +1100

    Refactor tests

--- logstash-output-firehose.gemspec
+++ logstash-output-firehose.gemspec
@@ -24,4 +24,5 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency "logstash-codec-line"
   s.add_runtime_dependency "logstash-codec-json_lines"
   s.add_development_dependency "logstash-devutils"
+  s.add_development_dependency "timecop"
 end
--- spec/outputs/firehose_spec.rb
+++ spec/outputs/firehose_spec.rb
@@ -1,39 +1,67 @@
 # encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
 require "logstash/outputs/firehose"
-require "logstash/codecs/plain"
 require "logstash/codecs/line"
 require "logstash/codecs/json_lines"
 require "logstash/event"
 require "aws-sdk"
+require "timecop"
 
 describe LogStash::Outputs::Firehose do
   dataStr = "123,someValue,1234567890"
 
   let(:sample_event) { LogStash::Event.new("message" => dataStr) }
-  let(:expected_event) { LogStash::Event.new("message" => dataStr) }
-  let(:output) { LogStash::Outputs::Firehose.new({"codec" => "plain"}) }
+  let(:time_now) { Time.now }
+  let(:expected_event) { "#{time_now.strftime("%FT%H:%M:%S.%3NZ")} %{host} 123,someValue,1234567890" }
+  let(:firehose_double) { instance_double(Aws::Firehose::Client) }
+  let(:stream_name) { "aws-test-stream" }
+  subject { LogStash::Outputs::Firehose.new({"codec" => "plain"}) }
 
   before do
     Thread.abort_on_exception = true
 
     # Setup Firehose client
-    output.stream = "aws-test-stream"
-    output.register
+    subject.stream = stream_name
+    subject.register
+
+    allow(Aws::Firehose::Client).to receive(:new).and_return(firehose_double)
+    allow(firehose_double).to receive(:put_record)
+    allow(firehose_double).to receive(:put_record_batch)
   end
 
-  describe "receive message with plain codec" do
-    subject {
-      expect(output).to receive(:handle_event) do |arg|
-        arg
-      end
-      output.receive(sample_event)
-    }
-
+  describe "receive one message" do
     it "returns same string" do
-      expect(subject).not_to eq(nil)
-      expect(subject.include? expected_event["message"]).to be_truthy
-      # expect(subject).to eq(expected_event["message"])
+      expect(firehose_double).to receive(:put_record).with({
+        delivery_stream_name: stream_name,
+        record: {
+            data: expected_event
+        }
+      })
+      Timecop.freeze(time_now) do
+        subject.receive(sample_event)
+      end
+    end
+  end
+
+  describe "receive multiple messages" do
+    it "returns same string" do
+      expect(firehose_double).to receive(:put_record_batch).with({
+        delivery_stream_name: stream_name,
+        record: [
+          {
+            data: expected_event
+          },
+          {
+            data: expected_event
+          },
+          {
+            data: expected_event
+          },
+        ]
+      })
+      Timecop.freeze(time_now) do
+        subject.multi_receive([sample_event, sample_event, sample_event])
+      end
     end
   end
 end
